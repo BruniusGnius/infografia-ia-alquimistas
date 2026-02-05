41534bfad32304a2467c81e7e278aa5ce1736907
BrunoPCH <bruno.perez.ch@gmail.com>
2026-02-05 17:31:16 -0600

    cambios

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/infografia-ia-alquimistas/', // 👈 ESTA ES LA CLAVE
   plugins: [react()],
+  base: '/infografia-ia-alquimistas/', // 👈 NOMBRE DEL REPO
 })
